8b620da77f9cc190e550405200d78e42cf7faacd
litao <lt.dygzs@qq.com>
2020-09-23 15:58:07 +0800

    增加注释

--- retrofit/src/main/java/retrofit2/RequestFactory.java
+++ retrofit/src/main/java/retrofit2/RequestFactory.java
@@ -322,6 +322,7 @@ final class RequestFactory {
         int p, Type parameterType, @Nullable Annotation[] annotations, boolean allowContinuation) {
       ParameterHandler<?> result = null;
       if (annotations != null) {
+        //by lt 在这里处理了函数的参数的默认注解(不使用注解就相当于用了@Field,但是只支持kt文件的interface),库中增加了kt反射
         if (annotations.length == 0) {
           result = RequestFactoryKtUtil.handlerParameterFromNoAnnotation(this, p, parameterType, annotations);
         } else {
