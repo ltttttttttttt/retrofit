e716b9d5985e36dccc531982879873bc97adef84
lt <1764780379@qq.com>
2021-03-05 17:36:41 +0800

    增加可以重试的call

---
+++ retrofit/src/main/java/retrofit2/call/RetryCall.kt
@@ -0,0 +1,69 @@
+package retrofit2.call
+
+import okhttp3.Request
+import okio.Timeout
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.io.IOException
+
+/**
+ * creator: lt  2021/3/5  lt.dygzs@qq.com
+ * effect : 用于处理重试请求
+ * warning:
+ */
+class RetryCall(private val retryNumber: Int, var call: Call<Any?>) : Call<Any?> {
+    private var retry = 0
+
+    override fun execute(): Response<Any?> {
+        while (retry <= retryNumber) {
+            retry++
+            try {
+                val t = call.execute()
+                if (t.isSuccessful && t.body() != null)
+                    return t
+            } catch (e: Exception) {
+                e.printStackTrace()
+                if (retry > retryNumber)
+                    throw e
+            }
+            call = call.clone()
+        }
+        throw IOException()
+    }
+
+    override fun enqueue(callback: Callback<Any?>) {
+        if (retry <= retryNumber) {
+            retry++
+            call.enqueue(object : Callback<Any?> {
+                override fun onResponse(c: Call<Any?>, response: Response<Any?>) {
+                    if (response.isSuccessful && response.body() != null)
+                        callback.onResponse(c, response)
+                    else {
+                        call = call.clone()
+                        enqueue(callback)
+                    }
+                }
+
+                override fun onFailure(c: Call<Any?>, t: Throwable) {
+                    call = call.clone()
+                    enqueue(callback)
+                }
+            })
+        } else {
+            callback.onFailure(this, IOException())
+        }
+    }
+
+    override fun clone(): Call<Any?> = call.clone()
+
+    override fun isExecuted(): Boolean = call.isExecuted
+
+    override fun cancel() = call.cancel()
+
+    override fun isCanceled(): Boolean = call.isCanceled
+
+    override fun request(): Request? = call.request()
+
+    override fun timeout(): Timeout = call.timeout()
+}
