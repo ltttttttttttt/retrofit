484f7dd8be65924906c63bdd8f40770dfe03b59a
lt <1764780379@qq.com>
2021-03-05 10:59:49 +0800

    Update OtherServiceMethod.kt

--- retrofit/src/main/java/retrofit2/OtherServiceMethod.kt
+++ retrofit/src/main/java/retrofit2/OtherServiceMethod.kt
@@ -7,6 +7,8 @@ import java.lang.reflect.Method
  * creator: lt  2021/3/3  lt.dygzs@qq.com
  * effect : 给用户提供更方便的动态代理方法转对象的功能,可以自定义Call<T>的生成
  * warning:
+ * [retrofit]Retrofit对象
+ * [method]需要生成的动态代理
  */
 abstract class OtherServiceMethod<T>(
         val retrofit: Retrofit,
@@ -26,15 +28,25 @@ abstract class OtherServiceMethod<T>(
         ): OtherServiceMethod<T>?
     }
 
+    /**
+     * 用于创建Call
+     * [url]HttpUrl,可以通过调用toString来获取String类型的url
+     * [requestParameterMap]请求参数键值对
+     * [args]动态代理请求该方法是传入的参数
+     *
+     * [url]都会传入,但是[requestParameterMap]只有POST和GET才会传入,其他需要自己获取参数(其实是我没用到过,而且太懒...)
+     * 返回Retrofit.Call对象,用于替换掉原生的OkHttp的Call
+     */
     abstract fun createCall(
             url: HttpUrl,
-            requestParameterMap: Map<String?, Any?>?
+            requestParameterMap: Map<String?, Any?>?,
+            args: Array<out Any>
     ): Call<T>
 
     override fun invoke(args: Array<out Any>): T? {
         val callAdapter = HttpServiceMethod.createCallAdapter<T, T?>(retrofit, method, method.genericReturnType, method.annotations)
         val request = requestFactory.create(args)
         val requestParameterMap = requestFactory.requestBuilder.getRequestParameterMap()
-        return callAdapter.adapt(createCall(request.url(), requestParameterMap))
+        return callAdapter.adapt(createCall(request.url(), requestParameterMap, args))
     }
 }
